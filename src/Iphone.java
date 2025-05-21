41659554481dabc6ee07563087e5249958b1eb28
Wermerson Macedo <wermersonm@gmail.com>
2025-05-20 23:09:10 -0300

    Criando todos os metodos do projeto.

--- src/Iphone.java
+++ src/Iphone.java
@@ -1,2 +1,47 @@
-public class Iphone {
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet{
+    @Override
+    public void tocar() {
+        System.out.println("Reproduzindo Musica!");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Musica Pausada!");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Musica " + musica + " selecionada.");
+
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo pagina "+url);
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando pagina atual!");
+    }
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada");
+    }
+
+    @Override
+    public void ligar(String numero){
+        System.out.println("Ligando para "+numero+" agora");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo ligacao");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
 }
--- src/ReprodutorMusical.java
+++ src/ReprodutorMusical.java
@@ -1,2 +1,7 @@
 public interface ReprodutorMusical {
+    void tocar();
+
+    void pausar();
+
+    void selecionarMusica(String musica);
 }
